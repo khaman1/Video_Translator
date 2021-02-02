0c68a666dc436b1b2654e77d55ba5be5b119b8ec
Kha <fanbsb90@gmail.com>
2021-02-02 11:08:00 +0700

    Update

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from library.video.video import Video
 subtitle = Video(
     fps=24,
     video_name='video 5',
-    stop_at_frame=12,
+    stop_at_frame=6,
     lang='chi_sim_new',
     )
 
 
-#subtitle.preview()
+subtitle.preview()
 subtitle = subtitle.extract_subtitle()
 
 subtitle.generate_version2()
